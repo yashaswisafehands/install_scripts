12658ddb82ad7af49c732aa2bfff190ac5570ed3
yashaswisafehands <yashaswi@afidigitalservices.com>
2025-05-30 10:39:19 +0530

    minute difference

--- lms.sh
+++ lms.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-echo "Creating tutor plugins dir"
-mkdir -p "$(tutor plugins printroot)"
+set -e  
 
-echo "transfering the premade plugins to the $(tutor plugins printroot)"
-cp /home/$USER/Desktop/install_scripts/fro-auth.py /home/$USER/.local/share/tutor-plugins/
+echo "Creating Tutor plugins directory..."
+TUTOR_PLUGIN_DIR=$(tutor plugins printroot)
+mkdir -p "$TUTOR_PLUGIN_DIR"
 
-echo "tutor config set"
+echo "Transferring custom plugin to $TUTOR_PLUGIN_DIR"
+cp /home/$USER/install_scripts/fro-auth.py "$TUTOR_PLUGIN_DIR/"
+
+echo "Running tutor config save..."
 tutor config save \
   --set LMS_HOST="local.openedx.io" \
   --set CMS_HOST="studio.local.openedx.io" \
@@ -13,31 +16,25 @@ tutor config save \
   --set CONTACT_EMAIL="$USER@afidigitalservices.com" \
   --set ENABLE_HTTPS="false"
 
+echo "Listing Tutor plugins..."
+tutor plugins list
 
-echo "tutor plugins list"
-echo "enabling custom auth"
-
+echo "Installing pluggy (required for plugin loading)..."
 pip install pluggy --break-system-packages
+
+echo "Enabling custom 'fro-auth' plugin..."
 tutor plugins enable fro-auth
 
-echo "building the mfe image"
-
+echo "Building MFE image..."
 tutor images build mfe
 
+echo "Launching Open edX platform locally..."
 tutor local launch --non-interactive
 
-echo "transfering custom indigo theme to the env/build openedx/themes"
+echo "Transferring custom Indigo theme to env/build/openedx/themes..."
+THEMES_DIR="/home/$USER/.local/share/tutor/env/build/openedx/themes"
+rm -rf "$THEMES_DIR/indigo"
+cp -r /home/$USER/Desktop/install_scripts/indigo "$THEMES_DIR/"
 
-rm -rf /home/$USER/.local/share/tutor/env/build/openedx/themes/indigo
-cp -r /home/$USER/Desktop/install_scripts/indigo /home/$USER/.local/share/tutor/env/build/openedx/themes/
-
-echo "building the image"
-
+echo "Building Open edX platform image with new theme..."
 tutor images build openedx
-
-
-
-
-
-# tutor config save --set RUN_MODE=production
-# tutor local launch --non-interactive
